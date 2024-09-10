5d62675d4be45198298297c5bb5a8e60373faf8f
Shane Freeder <theboyetronic@gmail.com>
2024-09-10 14:20:47 +0100

    Fix some issues with the seeding script

--- server/seed.js
+++ server/seed.js
@@ -24,8 +24,8 @@ async function addComment(destination, name, message, review) {
 }
 
 async function truncate() {
-    await pool.query("TRUNCATE project_destinations")
-    await pool.query("TRUNCATE FROM project_comments")
+    await pool.query("DELETE FROM project_comments")
+    await pool.query("DELETE FROM project_destinations")
 }
 
 //await truncate();
@@ -33,7 +33,7 @@ async function truncate() {
 await addDestination("Spain", "https://imgproxy.natucate.com/wF8RACmH64YN4YV3BSgRXmhB7LtrW2eBW-xf6wRpJmQ/rs:fill/aHR0cHM6Ly93d3cubmF0dWNhdGUuY29tL21lZGlhL3BhZ2VzL3JlaXNlemllbGUvNDI4YTYzZWUtMmYzOS00YmFjLTgwY2UtNmY2N2Y4Yzc1NzJlL2MxNDJhZjc3MWUtMTY3OTQ4Njc1MC9zcGFuaWVuLWxhZW5kZXJpbmZvcm1hdGlvbmVuLXN0YWR0LW96ZWFuLXdhc3Nlci1uYXR1Y2F0ZS5qcGc")
 await addDestination("France", "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg")
 await addDestination("Germany", "https://www.abercrombiekent.co.uk/-/media/abercrombieandkent/images/page-header-images/europe/germany/germany__0001_germany-berlin-brandenburg-gate_shutterstock_367652915.jpg?la=en&hash=114B9999E63136AA1541ADFA5A86A741BEEF5527");
-await addComment("spain", "John Smith", "Grate place!", 5);
+await addComment("Spain", "John Smith", "Grate place!", 5);
 
 
 
