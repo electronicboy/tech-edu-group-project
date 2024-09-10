7f7a27e226e32f81347d4ea2902c7541592a3e4e
Shane Freeder <theboyetronic@gmail.com>
2024-09-10 11:50:28 +0100

    Add Destinations GET route

--- server/server.js
+++ server/server.js
@@ -11,8 +11,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/** @type {Client} */
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
+})
+
+app.get("/destinations", (request, response) => {
+    /** @type {Result} */
+    pool.query("SELECT destination_id, destination_name, destination_img FROM project_destinations").then(results => {
+        if (results.rowCount > 0) {
+            const filtered = results.rows.map((entry) => {
+                return {
+                    id: entry.destination_id, name: entry.destination_name, img: entry.destination_img
+                }
+            })
+            response.json(filtered);
+        } else {
+            response.status(404).end();
+        }
+    })
 })
 
 
