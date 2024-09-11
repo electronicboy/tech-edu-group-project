134c1840d3084d4a999180a841a07f31431acaad
Shane Freeder <theboyetronic@gmail.com>
2024-09-11 11:12:28 +0100

    quick jsdoc fix

--- client/apiClient.js
+++ client/apiClient.js
@@ -3,7 +3,7 @@ const API = location.href.includes("localhost") ? "http://localhost:8081" : "htt
 
 /**
  *
- * @returns {Promise<{id: number, name: string, img: string|null}[]>}
+ * @returns {Promise<Array.<{id: number, name: string, img: string|null}>>}
  */
 async function getDestinations() {
     const response = await fetch(`${API}/destinations`)
