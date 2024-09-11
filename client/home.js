c2067d9eced7cff7e7d8aa8970db041c2284094e
Shane Freeder <theboyetronic@gmail.com>
2024-09-11 09:15:15 +0100

    Fix build error

--- client/home.js
+++ client/home.js
@@ -2,32 +2,37 @@ import {getDestinations} from "./apiClient";
 
 //toggle form animation
 document.addEventListener("DOMContentLoaded", function () {
-  const toggleFormButton = document.getElementById("toggle-form");
-  const formContent = document.getElementById("form-content");
+    const toggleFormButton = document.getElementById("toggle-form");
+    const formContent = document.getElementById("form-content");
 
-  toggleFormButton.addEventListener("click", function () {
-    if (formContent.style.display === "block") {
-      formContent.style.display = "none";
-    } else {
-      formContent.style.display = "block";
-    }
-  });
+    toggleFormButton.addEventListener("click", function () {
+        if (formContent.style.display === "block") {
+            formContent.style.display = "none";
+        } else {
+            formContent.style.display = "block";
+        }
+    });
 
-  reviewForm = document.getElementById("reviewForm");
+    reviewForm = document.getElementById("reviewForm");
 // handleSubmit code...
 });
 
-const imageContainer = document.getElementById("imageContainer")
+async function populate() {
 
-const destinations = await getDestinations();
-for (let destination of destinations) {
-console.log("dest", destination);
-const container = document.createElement("div");
-container.classList.add("box")
-const boxFooter = document.createElement("div");
-boxFooter.classList.add("box-footer");
-container.appendChild(boxFooter);
-boxFooter.innerText = destination.name;
-container.style.backgroundImage = `url('${destination.img}')`;
-imageContainer.appendChild(container);
+    const imageContainer = document.getElementById("imageContainer")
+
+    const destinations = await getDestinations();
+    for (let destination of destinations) {
+        console.log("dest", destination);
+        const container = document.createElement("div");
+        container.classList.add("box")
+        const boxFooter = document.createElement("div");
+        boxFooter.classList.add("box-footer");
+        container.appendChild(boxFooter);
+        boxFooter.innerText = destination.name;
+        container.style.backgroundImage = `url('${destination.img}')`;
+        imageContainer.appendChild(container);
+    }
 }
+
+populate()
