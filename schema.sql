a6aa256e20ddccb0aafcbe60fcb36ea779e4577e
Shane Freeder <theboyetronic@gmail.com>
2024-09-12 09:21:16 +0100

    Update schema to add likes

--- schema.sql
+++ schema.sql
@@ -11,5 +11,12 @@ CREATE TABLE IF NOT EXISTS project_comments (
     comment_date TIMESTAMP DEFAULT now(),
     comment_message TEXT,
     comment_review INT,
+    comment_likes INT DEFAULT 0,
     FOREIGN KEY (destination_id) REFERENCES project_destinations(destination_id)
 );
+
+-- ALTER TABLE project_comments ADD COLUMN comment_likes INT DEFAULT 0;
+
+-- DELETE FROM project_comments WHERE comment_id = ?;
+
+-- UPDATE project_comments SET comment_likes = comment_likes + 1 WHERE comment_id = ?;
